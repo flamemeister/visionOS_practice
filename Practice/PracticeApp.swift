12b974ae3e68924af0b47aaa080d25ce2c510beb
samxael <aldiyar.saken123@gmail.com>
2024-08-03 18:51:09 +0500

    Initial Commit

---
+++ Practice/PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeApp.swift
+//  Practice
+//
+//  Created by Aldiyar Saken on 03.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
